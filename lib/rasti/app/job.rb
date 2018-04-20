c68ae8f1616b75d90b43a00b0427f59cb7f218b3
Gabriel Naiman <gnaiman@keepcon.com>
2018-04-20 14:01:06 -0300

    Fixed: job session

--- lib/rasti/app/job.rb
+++ lib/rasti/app/job.rb
@@ -8,7 +8,8 @@ module Rasti
 
       def call
         raise "Undefined #{self.class.name}.environment" unless self.class.environment
-        
+
+        session = params[:session]
         session.job_id = @process.id
         interaction = params[:interaction].new self.class.environment, session
         interaction.call params[:interaction].build_form params[:params]
